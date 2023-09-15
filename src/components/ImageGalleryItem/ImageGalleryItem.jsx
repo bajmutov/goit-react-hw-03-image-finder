665f7c9bb11a3e53bb1d11fda67a2cb5bbbeb3e7
bajmutov <bajmutov@gmail.com>
2023-09-15 12:11:03 +0000

    Deploying to gh-pages from @ bajmutov/goit-react-hw-03-image-finder@a99e104dcb56c6a1e7b295bd2612e1b3371b62c1 🚀

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++
@@ -1,9 +0,0 @@
-const ImageGalleryItem = ({ photo: { tags, webformatURL } }) => {
-  return (
-    <li className="gallery-item">
-      <img src={webformatURL} alt={tags} />
-    </li>
-  );
-};
-
-export default ImageGalleryItem;
