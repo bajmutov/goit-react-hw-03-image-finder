da26d019381a3b852b7cf212c26882496e96b840
Oleksandr <bajmutov@gmail.com>
2023-09-15 14:57:09 +0300

    Update ImageGalleryItem.jsx

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,3 +1,9 @@
-<li class="gallery-item">
-  <img src="" alt="" />
-</li>;
+const ImageGalleryItem = ({ photo: { tags, webformatURL } }) => {
+  return (
+    <li className="gallery-item">
+      <img src={webformatURL} alt={tags} />
+    </li>
+  );
+};
+
+export default ImageGalleryItem;
