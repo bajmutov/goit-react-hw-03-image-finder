0abfcdcbc806285f5575b4a312a127f1653d25ee
Oleksandr <bajmutov@gmail.com>
2023-09-15 14:56:44 +0300

    Update ImageGallery.jsx

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,3 +1,11 @@
-<ul class="gallery">
-  <!-- Набір <li> із зображеннями -->
-</ul>
+import ImageGalleryItem from 'components/ImageGalleryItem/ImageGalleryItem';
+
+export const ImageGallery = ({ photos }) => {
+  return (
+    <ul className="gallery">
+      {photos.map(photo => (
+        <ImageGalleryItem key={photo.id} photo={photo} />
+      ))}
+    </ul>
+  );
+};
