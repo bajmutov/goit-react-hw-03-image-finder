4bd8daffc8b176b849e2bdafaa5c89d3df2259ac
Oleksandr <bajmutov@gmail.com>
2023-09-17 10:52:02 +0300

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,5 +1,51 @@
-<div class="overlay">
-  <div class="modal">
-    <img src="" alt="" />
-  </div>
-</div>;
+import { Component } from 'react';
+import { createPortal } from 'react-dom';
+
+const modalRoot = document.querySelector('#modal-root');
+
+export default class Modal extends Component {
+  componentDidMount() {
+    console.log('Modal componentDidMount');
+    window.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    console.log('Modal componentWillUnmount');
+    window.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  handleKeyDown = e => {
+    if (e.code === 'Escape') {
+      console.log('Нажали ESC, нужно закрыть модалку');
+
+      this.props.onClose();
+    }
+  };
+
+  handleBackdropClick = event => {
+    // console.log('Кликнули в бекдроп');
+
+    // console.log('currentTarget: ', event.currentTarget);
+    // console.log('target: ', event.target);
+
+    if (event.currentTarget === event.target) {
+      this.props.onClose();
+    }
+  };
+
+  render() {
+    console.log('first', this.props);
+    return createPortal(
+      <div className="Modal__backdrop" onClick={this.handleBackdropClick}>
+        <div className="Modal__content">
+          <img src={this.props.webformatURL} alt={this.props.tags} />
+        </div>
+      </div>,
+      modalRoot
+    );
+  }
+}
+
+// <div className="Modal__backdrop" onClick={this.handleBackdropClick}>
+//   <div className="Modal__content">{this.props.children}</div>
+// </div>;
